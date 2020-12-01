e6d549cfb2e5fb95bcc0cbdd5917ba2a96e5272e
Rafael Medeiros <rafael.medeiros@carenet.com.br>
2020-11-30 21:09:31 -0300

    port 80 can't be used for test, trying 443

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -9,4 +9,4 @@ export const HEADER_X_HRTIME: string = 'x-high-resolution-elapsed-time';
 export const DEFAULT_PACKAGE: string = 'package.json';
 export const UTF8: string = 'utf-8';
 export const DEFAULT_HTTP_PORT: number = 8181;
-export const DEFAULT_HTTPS_PORT: number = 4443;
+export const DEFAULT_HTTPS_PORT: number = 443;
