da970a178c39e0ef2ce1e8d41ebc8ffd8743c5ec
Rafael Medeiros <rafael.medeiros@carenet.com.br>
2021-07-08 13:23:54 -0300

    upgrade sonar-scanner

--- .circleci/install_sonar-scanner.sh
+++ .circleci/install_sonar-scanner.sh
@@ -3,8 +3,8 @@ if [ -d sonar-scanner-4.5.0.2216-linux ]
 then
     echo "Sonar Scanner already cached"
 else
-    curl -o scanner.zip 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.5.0.2216-linux.zip'
+    curl -o scanner.zip 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip'
     mkdir sonar-scanner
     unzip scanner.zip
-    chmod +x sonar-scanner-4.5.0.2216-linux/bin/sonar-scanner
+    chmod +x sonar-scanner-4.6.2.2472-linux/bin/sonar-scanner
 fi
